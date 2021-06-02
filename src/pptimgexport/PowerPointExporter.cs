a55b33d06a4e0513a5ba9cf00c108b2383ecb935
Takeshi Katano <takatano@outlook.com>
2021-06-03 01:04:40 +0900

    Initial code

---
+++ src/pptimgexport/PowerPointExporter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Office.Core;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace libppexport
+{
+    public class PowerPointExporter
+    {
+        public static void ExportAsPng(string powerPointFileFullPath, string outputFolderFullPath)
+        {
+            var ppApp = new PowerPoint.Application();
+            try
+            {
+                var slide = ppApp.Presentations.Open(powerPointFileFullPath, MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse);
+                slide.SaveCopyAs(outputFolderFullPath, PowerPoint.PpSaveAsFileType.ppSaveAsPNG, MsoTriState.msoTriStateMixed);
+            }
+            finally
+            {
+                ppApp.Quit();
+            }
+        }
+    }
+}
---
+++ src/slideimgdiff/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ppimgdiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
