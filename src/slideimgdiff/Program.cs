0e3d5c49a36ed78603641762112940e1b8686cbe
Takeshi Katano <takatano@outlook.com>
2021-06-04 00:23:10 +0900

    Add CLI implementation

--- src/slideimgdiff/Program.cs
+++ src/slideimgdiff/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using libppexport;
 
 namespace ppimgdiff
 {
@@ -6,6 +7,15 @@ namespace ppimgdiff
     {
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: ppimgdiff.exe PpFilePath OutputFolderPath");
+                return;
+            }
+            var ppFilePath = args[0];
+            var outputFolderPath = args[1];
+
+            PowerPointExporter.ExportAsPng(ppFilePath, outputFolderPath);
         }
     }
 }
