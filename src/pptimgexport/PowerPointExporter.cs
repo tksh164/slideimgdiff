b0132256f532d19d1148c819dba953656661de6c
Takeshi Katano <takatano@outlook.com>
2021-06-05 00:23:46 +0900

    Add rename exprted file code

--- src/pptimgexport/PowerPointExporter.cs
+++ src/pptimgexport/PowerPointExporter.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Office.Core;
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Office.Core;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 
 namespace libppexport
@@ -12,10 +14,28 @@ namespace libppexport
             {
                 var slide = ppApp.Presentations.Open(powerPointFileFullPath, MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse);
                 slide.SaveCopyAs(outputFolderFullPath, PowerPoint.PpSaveAsFileType.ppSaveAsPNG, MsoTriState.msoTriStateMixed);
+                slide.Close();
             }
             finally
             {
                 ppApp.Quit();
+            }
+
+            var regex = new Regex(@"^slide(?<num>[0-9]+)\.png$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            var options = new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                MatchCasing = MatchCasing.CaseInsensitive,
+                MatchType = MatchType.Simple,
+                RecurseSubdirectories = false,
+                ReturnSpecialDirectories = false,
+            };
+            foreach (var filePath in Directory.EnumerateFiles(outputFolderFullPath, "slide*.png", options))
+            {
+                var match = regex.Match(Path.GetFileName(filePath));
+                var num = uint.Parse(match.Groups["num"].Value);
+                var newFilePath = Path.Combine(Path.GetDirectoryName(filePath), string.Format("s{0:0000}.png", num));
+                File.Move(filePath, newFilePath, true);
             }
         }
     }
