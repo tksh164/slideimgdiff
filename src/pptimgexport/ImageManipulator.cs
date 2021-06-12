00e208d6bbd41a668c819acbeea11b871854d60f
Takeshi Katano <takatano@outlook.com>
2021-06-13 00:23:12 +0900

    Change to instance methods

--- src/pptimgexport/ImageManipulator.cs
+++ src/pptimgexport/ImageManipulator.cs
@@ -1,10 +1,21 @@
 ﻿using OpenCvSharp;
+using D = System.Drawing;
 
 namespace libppexport
 {
     public class ImageManipulator
     {
-        public static void SaveDiffDrawnImage(string inputImageFilePath1, string outputImageFilePath1, string inputImageFilePath2, string outputImageFilePath2)
+        public D.Size BlurSize { get; set; }
+
+        public D.Color FillColor { get; set; }
+
+        public ImageManipulator()
+        {
+            BlurSize = new D.Size(15, 15);
+            FillColor = D.Color.FromArgb(0, 255, 0);
+        }
+
+        public void SaveDiffDrawnImage(string inputImageFilePath1, string outputImageFilePath1, string inputImageFilePath2, string outputImageFilePath2)
         {
             using (var inputImage1 = new Mat(inputImageFilePath1, ImreadModes.Color))
             using (var inputImage2 = new Mat(inputImageFilePath2, ImreadModes.Color))
@@ -15,19 +26,24 @@ namespace libppexport
             }
         }
 
-        private static void DrawDiffContours(Mat image1, Mat image2)
+        private void DrawDiffContours(Mat image1, Mat image2)
         {
             Point[][] contours;
-            using (var absDiffImage = CreateAbsDiffImage(image1, image2))                                          // Create an abs diff image.
-            using (var grayscaleImage = absDiffImage.CvtColor(ColorConversionCodes.RGB2GRAY))                      // Convert to grayscale.
-            using (var blurImage = grayscaleImage.Blur(new Size(15, 15), new Point(-1, -1), BorderTypes.Default))  // Expand the diff areas by blur.
-            using (var binImage = blurImage.Threshold(1, 255, ThresholdTypes.Binary))                              // Create a black and white image.
+
+            // Create an abs diff image.
+            using (var absDiffImage = CreateAbsDiffImage(image1, image2))
+            // Convert to grayscale image.
+            using (var grayscaleImage = absDiffImage.CvtColor(ColorConversionCodes.RGB2GRAY))
+            // Expand the diff contours by blur.
+            using (var blurImage = grayscaleImage.Blur(new Size(BlurSize.Width, BlurSize.Height), new Point(-1, -1), BorderTypes.Default))
+            // Convert to black and white image.
+            using (var binImage = blurImage.Threshold(1, 255, ThresholdTypes.Binary))
             {
                 binImage.FindContours(out contours, out _, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
             }
 
             const int DrawAllContour = -1;
-            var contourColor = new Scalar(0, 255, 0);
+            var contourColor = new Scalar(FillColor.R, FillColor.G, FillColor.B);
             Cv2.DrawContours(image1, contours, DrawAllContour, contourColor, Cv2.FILLED, LineTypes.AntiAlias);
             Cv2.DrawContours(image2, contours, DrawAllContour, contourColor, Cv2.FILLED, LineTypes.AntiAlias);
         }
