e2395a9bf1786693ef54173ac34cf4e4d7e2b49b
Takeshi Katano <takatano@outlook.com>
2021-06-16 22:12:32 +0900

    Add option params class and change to static method

--- src/pptimgexport/DiffDrawnImageMaker.cs
+++ src/pptimgexport/DiffDrawnImageMaker.cs
@@ -3,30 +3,35 @@ using D = System.Drawing;
 
 namespace libppexport
 {
-    public class DiffDrawnImageMaker
+    public class DiffDrawnImageOptions
     {
         public D.Size BlurSize { get; set; }
 
         public D.Color FillColor { get; set; }
 
-        public DiffDrawnImageMaker()
+        public DiffDrawnImageOptions()
         {
             BlurSize = new D.Size(15, 15);
             FillColor = D.Color.FromArgb(0, 255, 0);
         }
+    }
 
-        public void SaveDiffDrawnImage(string inputImageFilePath1, string outputImageFilePath1, string inputImageFilePath2, string outputImageFilePath2)
+    public static class DiffDrawnImageMaker
+    {
+        public static void SaveDiffDrawnImage(string inputImageFilePath1, string outputImageFilePath1, string inputImageFilePath2, string outputImageFilePath2, DiffDrawnImageOptions options = null)
         {
+            if (options == null) options = new DiffDrawnImageOptions();
+
             using (var inputImage1 = new Mat(inputImageFilePath1, ImreadModes.Color))
             using (var inputImage2 = new Mat(inputImageFilePath2, ImreadModes.Color))
             {
-                DrawDiffArea(inputImage1, inputImage2);
+                DrawDiffArea(inputImage1, inputImage2, options);
                 inputImage1.SaveImage(outputImageFilePath1);
                 inputImage2.SaveImage(outputImageFilePath2);
             }
         }
 
-        private void DrawDiffArea(Mat image1, Mat image2)
+        private static void DrawDiffArea(Mat image1, Mat image2, DiffDrawnImageOptions options)
         {
             Point[][] contours;
 
@@ -35,7 +40,7 @@ namespace libppexport
             // Convert to a grayscale image.
             using (var grayscaleImage = absDiffImage.CvtColor(ColorConversionCodes.RGB2GRAY))
             // Expand the diff contours by blur.
-            using (var blurImage = grayscaleImage.Blur(new Size(BlurSize.Width, BlurSize.Height), new Point(-1, -1), BorderTypes.Default))
+            using (var blurImage = grayscaleImage.Blur(new Size(options.BlurSize.Width, options.BlurSize.Height), new Point(-1, -1), BorderTypes.Default))
             // Convert to a black and white image.
             using (var binImage = blurImage.Threshold(1, 255, ThresholdTypes.Binary))
             {
@@ -43,7 +48,7 @@ namespace libppexport
             }
 
             const int DrawAllContour = -1;
-            var contourFillColor = new Scalar(FillColor.R, FillColor.G, FillColor.B);
+            var contourFillColor = new Scalar(options.FillColor.B, options.FillColor.G, options.FillColor.R);
             Cv2.DrawContours(image1, contours, DrawAllContour, contourFillColor, Cv2.FILLED, LineTypes.AntiAlias);
             Cv2.DrawContours(image2, contours, DrawAllContour, contourFillColor, Cv2.FILLED, LineTypes.AntiAlias);
         }
