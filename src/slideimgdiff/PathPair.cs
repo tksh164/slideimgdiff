52eef925413aa17e7f51a7b3e999d84e95fd6b3c
Takeshi Katano <takatano@outlook.com>
2021-06-20 18:19:01 +0900

    Add protected access modifier

--- src/slideimgdiff/PathPair.cs
+++ src/slideimgdiff/PathPair.cs
@@ -2,8 +2,8 @@
 {
     internal class PathPair
     {
-        public string Path1 { get; set; }
-        public string Path2 { get; set; }
+        public string Path1 { get; protected set; }
+        public string Path2 { get; protected set; }
 
         public PathPair(string path1, string path2)
         {
